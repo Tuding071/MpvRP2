117e56a3f873d738c61fd815fd5922c0928cca5a
Tuding071 <davidpenaranda071@gmail.com>
2025-11-24 11:11:56 +0800

    Update MPVLib.kt

--- app/src/main/java/is/xyz/mpv/MPVLib.kt
+++ app/src/main/java/is/xyz/mpv/MPVLib.kt
@@ -4,9 +4,39 @@ import android.content.Context
 import android.view.Surface
 
 object MPVLib {
-    external fun create(ctx: Context)
-    external fun attachSurface(surface: Surface)
-    external fun detachSurface()
-    external fun command(command: Array<String>)
+    // Exact JNI functions from main.cpp
+    @JvmStatic
+    external fun create(appctx: Context)
+    
+    @JvmStatic
+    external fun init()
+    
+    @JvmStatic
     external fun destroy()
+    
+    @JvmStatic
+    external fun command(jarray: Array<String>)
+    
+    // These are probably in render.cpp based on typical mpv Android structure
+    @JvmStatic
+    external fun attachSurface(surface: Surface)
+    
+    @JvmStatic
+    external fun detachSurface()
+    
+    @JvmStatic
+    external fun setOptionString(name: String, value: String)
+    
+    // Helper functions
+    fun loadFile(path: String) {
+        command(arrayOf("loadfile", path))
+    }
+    
+    fun playPause() {
+        command(arrayOf("cycle", "pause"))
+    }
+    
+    fun stop() {
+        command(arrayOf("stop"))
+    }
 }
